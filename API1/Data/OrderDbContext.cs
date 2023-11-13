18f7a3f35480ff0cd8370c24de5408fe2d403706
letuananh1118 <tuananhlee1811@gmail.com>
2023-11-13 19:51:03 +0700

    nop bai

---
+++ API1/Data/OrderDbContext.cs
@@ -0,0 +1,21 @@
+﻿using API1.Model;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace API1.Data
+{
+    public class OrderDbContext : DbContext
+    {
+        public OrderDbContext(DbContextOptions<DbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+        public DbSet<Order> OrderTbl { get; set; }
+    }
+}
---
+++ API1/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API1.Model
+{
+    public class Order
+    {
+        [Key]
+        public int ItemCode { get; set; }
+
+        public string ItemName { get; set; } = string.Empty;
+
+        public string ItemQty { get; set; } = string.Empty;
+
+        public string OrderDelivery { get; set; } = string.Empty;
+
+        public string OrderAddress { get; set; } = string.Empty;
+
+        public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
