452a144f576e4e7cc062a5f13e0dfd01dbcd85e6
LamVanDuc <88825052+LamVanDuc@users.noreply.github.com>
2023-11-13 19:47:22 +0700

    Bai test

---
+++ Duclv/DMAWS_Test/DMAWS_Test/model/OrderSystemContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DMAWS_Test.model
+{
+    public class OrderSystemContext  : DbContext
+    {
+
+        public OrderSystemContext(DbContextOptions<OrderSystemContext> options) : base(options)
+        {
+
+        }
+        public DbSet<OrderTbl> OrderTbl { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Duclv/DMAWS_Test/DMAWS_Test/model/OrderTbl.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DMAWS_Test.model
+{
+    public class OrderTbl
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ItemCode { get; set; }
+
+        [Column(TypeName = "nvarchar(256)")]
+        public string ItemName { get; set; }
+
+        [Column(TypeName = "int")]
+        public int ItemQty { get; set; }
+
+        [Column(TypeName = "nvarchar(512)")]
+        public string OrderDelivery { get; set; }
+
+        [Column(TypeName = "nvarchar(512)")]
+        public string OrderAddress { get; set; }
+
+        [Column(TypeName = "nvarchar(30)")]
+        public string PhoneNumber { get; set; }
+    }
+}
