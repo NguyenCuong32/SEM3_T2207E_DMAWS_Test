25e647fd796f8feb1184af6cf4325267ee7193d0
NCanhhieu <109597988+NCanhhieu@users.noreply.github.com>
2023-11-13 20:01:41 +0700

    nop

--- Exam/cloudexamapi/Program.cs
+++ Exam/cloudexamapi/Program.cs
@@ -1,6 +1,28 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+string _policyName = "CorsPolicy";
+string _anotherPolicy = "AnotherCorsPolicy";
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy(name: _policyName, builder =>
+    {
+        builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+    opt.AddPolicy(name: _anotherPolicy, builder =>
+    {
+        builder.WithOrigins("https://localhost:7056")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+    });
+});
+// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -19,5 +41,13 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+app.UseCors(_policyName);
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
