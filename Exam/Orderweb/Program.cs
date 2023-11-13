914f160d3d35d46e7379963d02233c8f7a2d143a
NCanhhieu <109597988+NCanhhieu@users.noreply.github.com>
2023-11-13 20:04:30 +0700

    nop

--- Exam/Orderweb/Program.cs
+++ Exam/Orderweb/Program.cs
@@ -6,7 +6,29 @@ builder.Services.AddDbContext<OrderwebContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("OrderwebContext") ?? throw new InvalidOperationException("Connection string 'OrderwebContext' not found.")));
 
 // Add services to the container.
+string _policyName = "CorsPolicy";
+string _anotherPolicy = "AnotherCorsPolicy";
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
 
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy(name: _policyName, builder =>
+    {
+        builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+    opt.AddPolicy(name: _anotherPolicy, builder =>
+    {
+        builder.WithOrigins("https://localhost:7056")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+    });
+});
+// Add services to the container.
+builder.Services.AddControllersWithViews();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -20,6 +42,14 @@ var app = builder.Build();
     app.UseSwagger();
     app.UseSwaggerUI();
 //}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+app.UseCors(_policyName);
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
