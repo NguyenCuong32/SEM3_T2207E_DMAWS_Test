bf5dd41a084b1647278c9801dd05104af5f471de
phamngocdung92 <phamngoc.dung@vnpt.vn>
2023-11-13 19:53:00 +0700

    pnd

---
+++ phamngocdungFinalTest/Models/OrderModel.cs
@@ -0,0 +1,13 @@
+﻿namespace phamngocdungFinalTest.Models
+{
+    public class OrderModel
+    {
+        public int Id { get; set; }
+        public string ItemCode { get; set; }
+        public string ItemName { get; set; }
+        public int ItemQty { get; set; }
+        public string OrderDelivery { get; set; }
+        public string OrderAddress { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ phamngocdungFinalTest/OrderDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using phamngocdungFinalTest.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace phamngocdungFinalTest
+{
+    public class OrderDbContext : DbContext
+    {
+        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<OrderModel>().HasKey(x => x.Id);
+            modelBuilder.Entity<OrderModel>().Property(p => p.Id)
+                                            .ValueGeneratedOnAdd()
+                                            .HasDefaultValue(1);
+            modelBuilder.Entity<OrderModel>().ToTable("OrderTbl");
+        }
+        public DbSet<OrderModel> Orders { get; set; }
+    }
+}
